ef91847433335c925fbbd30983b171fc867f0867
Joao Guilherme <tertuliano@sunnit.com.br>
2020-10-17 20:36:44 -0300

    style: install angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { IssuerComponent } from './issuer/issuer.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { IssuerComponent } from './issuer/issuer.component';
     IssuerComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
