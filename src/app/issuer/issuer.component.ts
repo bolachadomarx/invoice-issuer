e8c769b72a3891e4753cf0032342c5ae171c844a
Joao Guilherme <tertuliano@sunnit.com.br>
2020-10-17 22:27:48 -0300

    feat: basic component to add invoices

--- src/app/issuer/issuer.component.ts
+++ src/app/issuer/issuer.component.ts
@@ -1,6 +1,14 @@
+import { IssuerService } from './../_service/issuer.service'
 import { ProductModel } from './../_models/product'
-import { Component, OnInit } from '@angular/core'
+import { Component, OnInit, ViewChild } from '@angular/core'
+import { MatPaginator } from '@angular/material/paginator'
+import { MatTable, MatTableDataSource } from '@angular/material/table'
+import { FormBuilder, FormGroup, Validators } from '@angular/forms'
 
+interface InvoiceProductModel {
+  product: ProductModel
+  quantity: number
+}
 @Component({
   selector: 'app-issuer',
   templateUrl: './issuer.component.html',
@@ -31,7 +39,59 @@ export class IssuerComponent implements OnInit {
     },
   ]
 
-  constructor() {}
+  @ViewChild(MatTable, { static: true }) table: MatTable<any>
+  dataSource: MatTableDataSource<InvoiceProductModel>
+  displayedColumns: string[] = ['code', 'description', 'value', 'discount', 'quantity', 'delete']
 
-  ngOnInit(): void {}
+  invoiceProducts: InvoiceProductModel[] = []
+  fb: FormBuilder = new FormBuilder()
+  invoiceForm: FormGroup
+  productInvoiceform: FormGroup
+  total: number = 0
+
+  constructor(private readonly issuerService: IssuerService) {
+    this.productInvoiceform = this.fb.group({
+      product: ['', Validators.required],
+      quantity: ['', Validators.required],
+    })
+
+    this.invoiceForm = this.fb.group({
+      client: ['', Validators.required],
+    })
+  }
+
+  ngOnInit(): void {
+    this.dataSource = new MatTableDataSource(this.invoiceProducts)
+  }
+
+  addProduct() {
+    const invoiceProduct = Object.assign(
+      {},
+      { product: this.productInvoiceform.get('product').value, quantity: this.productInvoiceform.get('quantity').value }
+    )
+    this.invoiceProducts.push(invoiceProduct)
+    this.dataSource.data = this.invoiceProducts
+    this.calcTotal()
+    this.productInvoiceform.reset()
+  }
+
+  calcTotal() {
+    this.total = 0
+    this.invoiceProducts.map((product) => {
+      const productValue = product.product.value * product.quantity
+      this.total = this.total + productValue
+    })
+  }
+
+  deleteProduct(index) {
+    this.invoiceProducts.splice(index, 1)
+    this.dataSource.data = this.invoiceProducts
+    this.calcTotal()
+  }
+
+  addInvoice() {
+    const date = new Date()
+    const invoiceCode = 'invoice' + this.invoiceForm.get('client').value + date.toISOString()
+    const invoice = Object.assign({}, this.invoiceForm.value, { products: this.invoiceProducts }, { code: invoiceCode })
+  }
 }
