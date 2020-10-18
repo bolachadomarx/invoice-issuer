1ac5e22208d99a772bb7d8a3327ddc78ac9d7e30
Joao Guilherme <tertuliano@sunnit.com.br>
2020-10-17 22:27:09 -0300

    chore: add angular material module to app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,6 @@
+import { IssuerService } from './_service/issuer.service'
+import { MaterialModule } from './material.module'
+import { RouterModule } from '@angular/router'
 import { AppRoutingModule } from './app-routing.module'
 import { BrowserModule } from '@angular/platform-browser'
 import { NgModule } from '@angular/core'
@@ -5,11 +8,21 @@ import { NgModule } from '@angular/core'
 import { AppComponent } from './app.component'
 import { IssuerComponent } from './issuer/issuer.component'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { FlexLayoutModule } from '@angular/flex-layout'
+import { ReactiveFormsModule } from '@angular/forms'
 
 @NgModule({
   declarations: [AppComponent, IssuerComponent],
-  imports: [BrowserModule, BrowserAnimationsModule],
-  providers: [AppRoutingModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    RouterModule,
+    AppRoutingModule,
+    MaterialModule,
+    FlexLayoutModule,
+    ReactiveFormsModule,
+  ],
+  providers: [IssuerService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
